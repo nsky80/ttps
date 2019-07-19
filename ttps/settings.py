3ee745af3e84a8217bffb5d2f27e67aab03a52de
Satish Kumar Yadav <42309618+nsky80@users.noreply.github.com>
2019-07-19 16:00:24 +0530

    Update settings.py

--- ttps/settings.py
+++ ttps/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y8b!tuslc65wr^!a1q3q&c-ud+*ri8m80ec&rr!f_f90qjz_%f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
