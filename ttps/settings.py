5cd874aa68097402411fe36fd4e21db607ff7fe9
Satish NSKY80 <satishkumary80@gmail.com>
2019-07-19 17:46:26 +0530

    Navbar font issue fixed

--- ttps/settings.py
+++ ttps/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '(u!04(j4tr$*xgt5gx@)4royb_dr=b)*(%uuov*jz8zoi3-h3o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
